1fd7000e0b5e9ea5740d5ee9cf3f2815ae668e89
aheff20 <aidan.m.heffron@gmail.com>
2024-03-24 17:11:59 -0500

    Update client.go

--- client.go
+++ client.go
@@ -1,25 +1,78 @@
 package main
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
 	"fmt"
+	"io"
 	"net"
 )
 
 func main() {
-	// Connect to the server
-	conn, err := net.Dial("tcp", "localhost:8080")
+	key := []byte("example key 1234")
+
+	conn, err := net.Dial("tcp", "127.0.0.1:9080")
+
 	if err != nil {
-		fmt.Println(err)
+		panic(err)
+	}
+
+	defer func() {
+		fmt.Println("Bye")
+
+		conn.Close()
+	}()
+
+	block, cipherErr := aes.NewCipher(key)
+
+	if cipherErr != nil {
+		fmt.Errorf("Can't create cipher:", cipherErr)
+
 		return
 	}
 
-	// Send some data to the server
-	_, err = conn.Write([]byte("Hello, server!"))
-	if err != nil {
-		fmt.Println(err)
+	iv := make([]byte, aes.BlockSize)
+
+	if _, randReadErr := io.ReadFull(rand.Reader, iv); randReadErr != nil {
+		fmt.Errorf("Can't build random iv", randReadErr)
+
 		return
 	}
 
-	// Close the connection
-	conn.Close()
+	_, ivWriteErr := conn.Write(iv)
+
+	if ivWriteErr != nil {
+		fmt.Errorf("Can't send IV:", ivWriteErr)
+
+		return
+	} else {
+		fmt.Println("IV Sent:", iv)
+	}
+
+	stream := cipher.NewCFBEncrypter(block, iv)
+
+	data := [][]byte{
+		[]byte("Test one"),
+		[]byte("Hello crypto"),
+		[]byte("Hello word"),
+		[]byte("Hello excel"),
+		[]byte("Hello powerpoint"),
+	}
+
+	for _, d := range data {
+		encrypted := make([]byte, len(d))
+
+		stream.XORKeyStream(encrypted, d)
+
+		writeLen, writeErr := conn.Write(encrypted)
+
+		if writeErr != nil {
+			fmt.Errorf("Write failed:", writeErr)
+
+			return
+		}
+
+		fmt.Println("Encrypted Data Written:", string(d), encrypted, writeLen)
+	}
 }
