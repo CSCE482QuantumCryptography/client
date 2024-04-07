9c0fb7504d32139ead56fe016f0481c9fda9f6db
aheff20 <aidan.m.heffron@gmail.com>
2024-04-07 13:13:12 -0500

    fixed timing issue

--- create_csr.go
+++ create_csr.go
@@ -4,7 +4,6 @@ import (
 	"encoding/binary"
 	"fmt"
 	"os"
-	"time"
 
 	"github.com/CSCE482QuantumCryptography/qs509"
 )
@@ -13,12 +12,8 @@ func CreateCsr() ([]byte, []byte, error) {
 	var sa qs509.SignatureAlgorithm
 	sa.Set(*signingAlg)
 
-	signCsrStart := time.Now()
 	qs509.GenerateCsr(sa, "client_private_key.key", "client_csr.csr")
 	qs509.SignCsr("./client_csr.csr", "client_signed_crt.crt", *caCert, *caKey)
-	signCsrEnd := time.Now()
-
-	timeMap["signCsr"] = []time.Time{signCsrStart, signCsrEnd}
 
 	clientCertFile, err := os.ReadFile("client_signed_crt.crt")
 	if err != nil {
